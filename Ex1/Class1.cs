7e5d2f4c25a8eec53c4c1c705299fd388299d371
Lucas Denipoti <lucasfdenipoti@gmail.com>
2020-09-16 17:49:16 -0300

    Adicionar arquivos de projeto.

---
+++ Ex1/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Ex1
+{
+    class Retangulo
+    {
+        public double Largura; 
+        public double Altura;
+        public double Area()
+        {
+            return Largura * Altura;
+        } 
+        public double Perimetro()
+        {
+            return (Largura * 2) + (Altura * 2);
+        }
+        public double Diagonal()
+        {
+            return Math.Sqrt((Largura * Largura) + (Altura * Altura));
+        }
+    }
+}
---
+++ Ex1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo L = new Retangulo();
+
+            Console.WriteLine("Entre com a Largura e Altura do Retângulo: ");
+            L.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            L.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        }
+    }
+}
